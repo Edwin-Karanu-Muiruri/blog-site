67258e4e9b3be75771ed06553507aa50ce024a22
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-05-11 21:22:21 +0300

    Created the view function for the sign up form and return a signup form.

--- app/authentication/views.py
+++ app/authentication/views.py
@@ -1,6 +1,20 @@
 from flask import render_template
 from . import authentication
+from ..models import User
+from .forms import SignUpForm
+from .. import db
 
 @authentication.route('/login')
 def login():
     return render_template('authentication/login.html')
+
+@authentication.route('/signup', methods = ["GET","POST"])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,username=form.username.data,password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('authentication.login'))
+        title = "Sign Up"
+    return render_template('authentication/register.html',signup_form = form)
